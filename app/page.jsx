5d5c1320b136fb3b24e0438f7042ebbe713afd26
Amanda Mesquita <amandamesquita73638@gmail.com>
2025-03-22 23:03:32 -0300

    Add Home component with sidebar and dynamic messaging layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 "use client"; // use client é uma diretiva que indica que o código deve ser executado no lado do cliente
 
 import { assets } from "@/assets/assets";
+import SideBar from "@/components/sideBar";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -12,20 +13,21 @@ function Home() {
   return (
     <div>
       <div className="flex h-screen">
-        <div className="flex flex-1 flex-col bg-[#292a2d] items-center justify-center px-4 pb-8 text-white relative">
+        <SideBar expand={expand} setExpand={setExpand} />
+        <div className="flex-1 flex flex-col bg-[#292a2d] items-center justify-center px-4 pb-8 text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image
               onClick={() => (expand ? setExpand(false) : setExpand(true))}
-              className="rotate-180"
               src={assets.menu_icon}
+              className="rotate-180"
               alt=""
-            ></Image>
-            <Image className="opacity-70" src={assets.chat_icon} alt=""></Image>
+            />
+            <Image src={assets.chat_icon} alt="" className="opacity-70" />
           </div>
           {messages.length === 0 ? (
             <>
               <div className="flex items-center gap-3">
-                <Image className="h-16" src={assets.logo_icon} alt=""></Image>
+                <Image src={assets.logo_icon} alt="" className="h-16" />
                 <p className="text-2xl font-medium">Hi, I&apos;m DeepSeek</p>
               </div>
               <p className="text-sm mt-4">How can I help you today?</p>

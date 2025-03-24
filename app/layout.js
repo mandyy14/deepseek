8cdf08f61b51b480f166d8c398e380c2de619c0e
Amanda Mesquita <amandamesquita73638@gmail.com>
2025-03-23 23:27:44 -0300

    Add AI chat route and integrate DeepSeek API for message generation

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { AppContextProvider } from "@/context/appContext";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 const inter = Inter({
@@ -18,7 +19,15 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <AppContextProvider>
         <html lang="en">
-          <body className={`${inter.className}  antialiased`}>{children}</body>
+          <body className={`${inter.className}  antialiased`}>
+            <Toaster
+              toastOptions={{
+                success: { style: { background: "black", color: "white" } },
+                error: { style: { background: "black", color: "white" } },
+              }}
+            />
+            {children}
+          </body>
         </html>
       </AppContextProvider>
     </ClerkProvider>
